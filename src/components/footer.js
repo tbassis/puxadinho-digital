f27d8b564039780328a2a025e9d99ad22dc347ed
tbassis <tulioassis.18@gmail.com>
2025-03-07 19:23:27 -0300

    Add a responsive footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -23,26 +23,35 @@ const paddingXFooter = "32px";
 
 // create a footer class, and clone the content of the template into it
 class footer extends HTMLElement {
-    constructor() {
-        super()
-        this.attachShadow({ mode: 'open' })
+	static observedAttributes = ["page-width"];
 
-        // This observer will react according to the size of the parent component
-        const resizeObserver = new ResizeObserver(entries => {
-            for (let entry of entries) {
-                const { width } = entry.contentRect;
-                this.style.width = `${width}px`; // Define a largura do Custom Element
-                this.style.paddingLeft = paddingXFooter;
-                this.style.paddingRight = paddingXFooter;
-            }
-        });
+	constructor() {
+		super()
+		this.attachShadow({ mode: 'open' })
 
-        // Initiate the father component observer
-        if (this.parentElement) {
-            resizeObserver.observe(this.parentElement);
-        }
-        this.shadowRoot.appendChild(footerTemplate.content.cloneNode(true))
-    }
+		// This observer will react according to the size of the parent component
+		const resizeObserver = new ResizeObserver(entries => {
+			for (let entry of entries) {
+				const { width } = entry.contentRect;
+				this.style.width = `${width}px`; // Define a largura do Custom Element
+				this.style.paddingLeft = paddingXFooter;
+				this.style.paddingRight = paddingXFooter;
+			}
+		});
+
+		// Initiate the father component observer
+		if (this.parentElement) {
+			resizeObserver.observe(this.parentElement);
+		}
+		this.shadowRoot.appendChild(footerTemplate.content.cloneNode(true))
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		this.style.width = `${newValue}px`; // Define a largura do Custom Element
+		console.log(
+			`Attribute ${name} has changed from ${oldValue} to ${newValue}.`,
+		);
+	}
 }
 
 // define a custom element called 'footer-component' using the footer class
