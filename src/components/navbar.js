ffa87d36c3d60fd792597ff7d41a86a0f07569ad
tbassis <tulioassis.18@gmail.com>
2025-03-19 14:29:40 -0300

    Improve file formmating, update names of the components menu and remove unused code

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,53 @@
-// crafted by Tulio Assis
+// Created by Tulio Assis
+// navbar.js implements the Web Component navbar-component, which is
+// The Custom Element style its defined declaratively
 
-const headerTemplate = document.createElement('template')
+// shadow root of the custom element
+const navbarTemplate = document.createElement("template")
+// declarative style of the component
 const style = document.createElement("style");
 
-headerTemplate.innerHTML = `
+class navbar extends HTMLElement {
+  // define a observer to listed attributes and trigger attributeChangedCallback when the value change
+  static get observedAttributes () {
+    return ["page-width"];
+  } 
+
+  constructor() {
+    // Always call super first in constructor
+    super()
+  }
+
+  connectedCallback() {
+    // Create a shadow root. The custom element itself is the shadow host
+    this.attachShadow({ mode: 'open' })
+
+    // Attach to the shadow tree the template and style element
+    this.shadowRoot.appendChild(navbarTemplate.content)
+    this.shadowRoot.appendChild(style);
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    // Set custom element's width equal to the parent's width 
+    this.style.width = `${newValue}px`; 
+  }
+}
+
+// custom element layout
+navbarTemplate.innerHTML = `
 <div class="nav-container">
   <h1>Puxadinho Digital</h1>
   <nav>
     <a id="nav-link-index" href="index.html" data-lang="pt-BR">Entrada</a>
+    <a id="nav-link-contact" href="dev.html" data-lang="pt-BR">Escritório</a>
     <a id="nav-link-blog" href="blog.html" data-lang="pt-BR">Biblioteca</a>
-    <a id="nav-link-photos" href="photos.html" data-lang="pt-BR">Fotos</a>
+    <a id="nav-link-photos" href="photos.html" data-lang="pt-BR">Galeria</a>
     <a id="nav-link-about" href="about.html" data-lang="pt-BR">Sobre</a>
-    <a id="nav-link-contact" href="contact.html" data-lang="pt-BR">Contato</a>
   </nav>
 </div>
 `
 
+// custom element style
 style.textContent = `
 .nav-container {
     display: flex;
@@ -74,13 +106,16 @@ style.textContent = `
       justify-content: center;
       padding: 10px 0px;
     }
+
     h1 {
       font-size: medium;
       padding-left: 16px;
     }
+
     nav {
       padding-right: 0px;
     }
+
     a {
       letter-spacing: 0.1em;
       padding: 5px;
@@ -90,37 +125,5 @@ style.textContent = `
 
 // create a header class, and clone the content of the template into it
 // add an observer to make the component size responsive
-class navbar extends HTMLElement {
-  static observedAttributes = ["page", "page-width"];
-
-  constructor() {
-    // Always call super first in constructor
-    super()
-    console.log("entroiu construtor");
-  }
-
-  connectedCallback() {
-    console.log("Custom element added to page.");
-    // Create a shadow root
-    this.attachShadow({ mode: 'open' })
-
-    // Attach the created elements to the shadow dom
-    this.shadowRoot.appendChild(headerTemplate.content.cloneNode(true))
-    this.shadowRoot.appendChild(style);
-  }
-
-  disconnectedCallback() {
-    console.log("Custom element removed from page.");
-  }
-
-  adoptedCallback() {
-    console.log("Custom element moved to new page.");
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    this.style.width = `${newValue}px`; // set the width of the custom element
-  }
-}
-
 // define a web component called 'navbar-component' using the header class
 customElements.define('navbar-component', navbar)
